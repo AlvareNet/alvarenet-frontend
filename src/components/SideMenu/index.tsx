ca0722ea89c3f57e8d49b8f90fcb1b3bf5acab2a
SileNce2web <andre.freese@icloud.com>
2021-09-17 15:33:57 +0200

    Basic Design

--- src/components/SideMenu/index.tsx
+++ src/components/SideMenu/index.tsx
@@ -1,15 +1,41 @@
-import { AppBar, Box, Drawer } from "@material-ui/core";
+import { Box, Drawer, List, ListItem, ListItemIcon, ListItemText } from "@material-ui/core";
+import { Home, SwapHoriz } from "@material-ui/icons";
 
-export default function SideMenu(){
-    return  (
+export default function SideMenu() {
+    return (
         <>
-        <Box sx={{ display: 'flex' }}>
-            <Drawer variant="permanent" sx={{ 
-                zIndex: (theme) => theme.zIndex.appBar - 1,
-                flexShrink: 0 }} >
-             "Test"   
+            <Drawer
+                variant="permanent"
+                sx={{
+                    zIndex: (theme) => theme.zIndex.appBar - 1,
+                    flexShrink: 0,
+                    ['.MuiDrawer-paper']: {
+                        width: '200px !important',
+                        height: 'calc(100% - 200px)',
+                        marginTop: '200px',
+                        borderTop: '1px solid rgba(0,0,0,0.12)',
+                        borderTopRightRadius: '25px',
+                        WebkitBoxShadow: "10px -5px 10px 0px rgba(0,0,0,0.1)",
+                        boxShadow: "10px -5px 10px 0px rgba(0,0,0,0.1)",
+                    }
+                }} >
+                <Box>
+                    <List>
+                        <ListItem button key={"Home"}>
+                            <ListItemIcon>
+                                <Home />
+                            </ListItemIcon>
+                            <ListItemText primary={"Home"} />
+                        </ListItem>
+                        <ListItem button key={"Swap"}>
+                            <ListItemIcon>
+                                <SwapHoriz />
+                            </ListItemIcon>
+                            <ListItemText primary={"Swap"} />
+                        </ListItem>
+                    </List>
+                </Box>
             </Drawer>
-        </Box>
         </>
     )
 }
