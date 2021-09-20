5a7cd164f8be7cfca76fb9dbeb6c9f0019d42ba3
SileNce2web <andre.freese@icloud.com>
2021-09-20 16:07:57 +0200

    Responsive Design & Page Navigation

--- src/components/Claim/index.tsx
+++ src/components/Claim/index.tsx
@@ -1,31 +1,48 @@
-import { Button } from "@material-ui/core"
+import { Button, Box, Grid } from "@material-ui/core"
 import { useClaimCallback, useUserHasAvailableClaim, useUserUnclaimedAmount } from "../../state/claim/slothi/hooks"
+import AlvareNet_Logo from "../../assets/images/AlvareNet_Logo.png"
 
-export default function Claim(){
-    const number = useUserUnclaimedAmount()
-    const available = useUserHasAvailableClaim()
-    const { claimCallback } = useClaimCallback()
-    function onClaim() {
-      claimCallback().then((result) => console.log(result))
-        // reset modal and log error
-        .catch((error) => {
-          console.log(error)
-        })
-    }
-    
+export default function Claim() {
+  const number = useUserUnclaimedAmount()
+  const available = useUserHasAvailableClaim()
+  const { claimCallback } = useClaimCallback()
+  function onClaim() {
+    claimCallback().then((result) => console.log(result))
+      // reset modal and log error
+      .catch((error) => {
+        console.log(error)
+      })
+  }
 
-    if(available){
-    return(
+
+  if (available) {
+    return (
+      <Box>
+        <img src={AlvareNet_Logo} alt="" />
         <Button onClick={onClaim}>
-        {"Claim " + number.toString() }
+          {"Claim " + number.toString()}
         </Button>
+      </Box>
+
     );
-    }
-    else{
-    return(
-        <div>
-        No tokens to claim!
-        </div>
+  }
+  else {
+    return (
+      <Grid container justifyContent="center">
+        <Grid item md={12} xs={12}>
+          <Grid container justifyContent="space-around">
+            <Grid item md={12} sx={{
+              backgroundImage: `url(${AlvareNet_Logo})`,
+              backgroundSize: 'contain',
+              backgroundRepeat: 'no-repeat',
+              backgroundPosition: 'center',
+              width: '200px',
+              height: '200px'
+            }}>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
     )
-    }
+  }
 }
