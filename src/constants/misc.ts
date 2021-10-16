776d8da518fcf0db38a4e63417f92bf9c66b5eff
leonardmgh <leonard@hansen-net.eu>
2021-10-16 22:03:16 +0200

    Chanded api address

--- src/constants/misc.ts
+++ src/constants/misc.ts
@@ -1,6 +1,6 @@
 export const NetworkContextName = 'NETWORK'
 
-export const ReflectionEndPoint = 'http://85.114.131.207:1337/api/transcactionbalance?wallet='
+export const ReflectionEndPoint = 'https://api.alvare.net/api/transcactionbalance?wallet='
 
 export const CLAIMMAPPINGURL: { MAINNET: string } = {
     MAINNET : process.env.PUBLIC_URL + '/airdropdata/mappings.json',
