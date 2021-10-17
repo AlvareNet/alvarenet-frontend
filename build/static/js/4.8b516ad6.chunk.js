93da284c50ba689e24cab74f5f2a52e594f6714b
leonardmgh <leonard@hansen-net.eu>
2021-10-17 21:08:05 +0200

    updated build

--- build/static/js/4.8b516ad6.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpalvarenet-frontend"]=this["webpackJsonpalvarenet-frontend"]||[]).push([[4],{470:function(n,t){},521:function(n,t){},523:function(n,t){},547:function(n,t){},549:function(n,t){},561:function(n,t){},579:function(n,t){},581:function(n,t){}}]);
-//# sourceMappingURL=4.8b516ad6.chunk.js.map
